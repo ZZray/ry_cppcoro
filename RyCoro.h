99aa69e827242e98f63dcca7df9e5ec539f54a84
Ray Zhang <1789958884@qq.com>
2024-07-17 23:49:44 +0800

    feat: todo suspend never支持嵌套

--- RyCoro.h
+++ RyCoro.h
@@ -5,328 +5,349 @@
  */
 #pragma once
 
+#include <atomic>
+#include <condition_variable>
 #include <coroutine>
+#include <future>
 #include <iostream>
 #include <queue>
 #include <thread>
-#include <condition_variable>
-#include <atomic>
-#include <future>
 
-template <typename T>
+template <typename T, bool InitialSuspend>
 class RyCoroTask;
-namespace RyCoro::detail
-{
+namespace RyCoro::detail {
 
-	template <typename T>
-	struct RyCoroTaskPromiseBase : std::promise<T>
-	{
-		std::exception_ptr exceptionPtr;
-		std::shared_ptr<std::atomic<bool>> detached = std::make_shared<std::atomic<bool>>(false);
-		std::coroutine_handle<> continuation = std::noop_coroutine(); // 谁在等待我
+template <typename T, bool InitialSuspend>
+struct RyCoroTaskPromiseBase : std::promise<T> {
+    std::exception_ptr exceptionPtr;
+    std::shared_ptr<std::atomic<bool>> detached = std::make_shared<std::atomic<bool>>(false);
+    std::coroutine_handle<> continuation = std::noop_coroutine(); // 谁在等待我
 
-		RyCoroTask<T> get_return_object();
-		std::suspend_always initial_suspend()
-		{
-			std::cout << "initial suspend\n";
-			return {};
-		}
+    RyCoroTask<T, InitialSuspend> get_return_object();
+    std::suspend_always initial_suspend()
+        requires InitialSuspend
+    {
+        std::cout << "initial suspend always\n";
+        return {};
+    }
 
-		struct FinalAwaiter
-		{
-			bool await_ready() const noexcept { return false; }
+    std::suspend_never initial_suspend()
+        requires !InitialSuspend
+    {
+        std::cout << "initial suspend never\n";
+        return {};
+    }
 
-			template <typename promise_type>
-			std::coroutine_handle<> await_suspend(std::coroutine_handle<promise_type> h) const noexcept
-			{
-				std::cout << "await_suspend " << h.address();
-				auto detached = h.promise().detached;
-				auto hContinuation = std::move(h.promise().continuation);
-				if (detached && detached->load()) {
-					std::cout << " detached destroy now\n";
-					h.destroy();
-				}
-				else {
-					std::cout << " not detached\n";
-				}
-				// 当前协程结束的时候，返回并恢复父协程。同样在task中，co_await 子协程的时候，会让子协程恢复执行
-				if (hContinuation) {
-					return hContinuation;
-				}
-				return std::noop_coroutine();
-			}
-			void await_resume() const noexcept { }
-		};
-		FinalAwaiter final_suspend() const noexcept
-		{
-			std::cout << "final_suspend\n";
-			;
-			return {};
-		}
+    struct FinalAwaiter {
+        bool await_ready() const noexcept { return false; }
 
-		void unhandled_exception()
-		{
-			exceptionPtr = std::current_exception();
-			this->set_exception(exceptionPtr);
-		}
-	};
+        template <typename promise_type>
+        std::coroutine_handle<> await_suspend(std::coroutine_handle<promise_type> h) const noexcept
+        {
+            std::cout << "await_suspend " << h.address();
+            auto detached = h.promise().detached;
+            auto hContinuation = std::move(h.promise().continuation);
+            if (detached && detached->load()) {
+                std::cout << " detached destroy now\n";
+                h.destroy();
+            } else {
+                std::cout << " not detached\n";
+            }
+            // 当前协程结束的时候，返回并恢复父协程。同样在task中，co_await 子协程的时候，会让子协程恢复执行
+            if (hContinuation) {
+                return hContinuation;
+            }
+            return std::noop_coroutine();
+        }
+        void await_resume() const noexcept { }
+    };
+    FinalAwaiter final_suspend() const noexcept
+    {
+        std::cout << "final_suspend\n";
+        ;
+        return {};
+    }
 
-	template <typename T>
-	struct RyCoroTaskPromise : RyCoro::detail::RyCoroTaskPromiseBase<T>
-	{
-		RyCoroTask<T> get_return_object();
-
-		// co_return, promise_type.return_value(total)
-		void return_value(T v)
-		{
-			std::cout << "return_value " << v << '\n';
-			this->set_value(std::move(v));
-		}
-	};
-
-	template <>
-	struct RyCoroTaskPromise<void> : RyCoroTaskPromiseBase<void>
-	{
-		RyCoroTask<void> get_return_object();
-
-		void return_void()
-		{
-			std::cout << "return_void\n";
-			this->set_value();
-		}
-	};
-
-} // namespace RyCoro::detail
-
-template <typename T>
-class RyCoroTaskBase
-{
-public:
-	using promise_type = RyCoro::detail::RyCoroTaskPromise<T>;
-
-	RyCoroTaskBase(std::coroutine_handle<promise_type> h)
-		: m_detached(h.promise().detached)
-		, m_coro(h)
-	{ }
-	~RyCoroTaskBase()
-	{
-		if (m_coro) {
-			if (m_coro.done()) {
-				std::cout << "destroy coro: " << m_coro.address() << '\n';
-				m_coro.destroy();
-			}
-			else {
-				std::cout << "detach coro: " << m_coro.address() << '\n';
-				detach();
-			}
-		}
-	}
-	RyCoroTaskBase(const RyCoroTaskBase&) = delete;
-	RyCoroTaskBase& operator=(const RyCoroTaskBase&) = delete;
-
-	RyCoroTaskBase(RyCoroTaskBase&& other) noexcept
-		: m_detached(std::move(other.m_detached))
-		, m_coro(other.m_coro)
-	{
-		other.m_coro = nullptr;
-	}
-
-	RyCoroTaskBase& operator=(RyCoroTaskBase&& other) noexcept
-	{
-		if (this != &other) {
-			if (m_coro && !m_detached->load()) {
-				m_coro.destroy();
-			}
-			m_coro = other.m_coro;
-			m_detached = std::move(other.m_detached);
-			other.m_coro = nullptr;
-		}
-		return *this;
-	}
-
-	// 获取协程句柄
-	std::coroutine_handle<> handle() { return m_coro; }
-
-	// 获取 shared_future，可以用于异步获取结果
-	std::shared_future<T> future()
-	{
-		if (!m_future.valid()) {
-			m_future = m_coro.promise().get_future().share();
-		}
-		return m_future;
-	}
-
-	// 检查是否有异常
-	bool hasException() const { return m_coro.promise().exceptionPtr != nullptr; }
-
-	// 新增：获取异常（如果有）
-	std::exception_ptr exception() const { return m_coro.promise().exceptionPtr; }
-
-	//
-	void resume() const
-	{
-		if (this->m_coro && !this->m_coro.done() && !this->m_started) {
-			this->m_coro.resume();
-		}
-	}
-	//
-	void detach() const { m_detached->store(true); }
-
-	///
-	bool await_ready() { return m_coro && m_coro.done(); }
-
-	T await_resume()
-	{
-		if (hasException()) {
-			return {};
-		}
-		return future().get();
-	}
-
-	// 挂起当前协程，并恢复子协程
-	std::coroutine_handle<> await_suspend(std::coroutine_handle<> waiter) noexcept
-	{
-		// 储存父协程的句柄
-		this->m_coro.promise().continuation = waiter;
-		if (!this->m_started) {
-			this->m_started = true;
-			return this->m_coro;
-		}
-		// 返回子协程的句柄，使其恢复执行
-		return std::noop_coroutine();
-	}
-
-protected:
-	std::shared_ptr<std::atomic<bool>> m_detached;
-	std::coroutine_handle<promise_type> m_coro;
-	std::shared_future<T> m_future;      // 缓存 shared_future 对象
-	std::atomic<bool> m_started = false; // 标记协程是否已经开始执行
+    void unhandled_exception()
+    {
+        exceptionPtr = std::current_exception();
+        this->set_exception(exceptionPtr);
+    }
 };
 
-template <typename T>
-class RyCoroTask : public RyCoroTaskBase<T>
-{
-public:
-	using RyCoroTaskBase<T>::RyCoroTaskBase;
+template <typename T, bool InitialSuspend = true>
+struct RyCoroTaskPromise : RyCoro::detail::RyCoroTaskPromiseBase<T, InitialSuspend> {
+    RyCoroTask<T, InitialSuspend> get_return_object();
 
-	// 获取协程结果 - 会阻塞当前线程
-	std::optional<T> waitForResult(std::chrono::milliseconds timeout = std::chrono::milliseconds(3000))
-	{
-		if (!this->m_coro) {
-			return std::nullopt;
-		}
-		if (!this->m_coro.done()) {
-			if (!this->m_started) {
-				this->resume();
-			}
-			auto status = this->future().wait_for(timeout);
-			if (status == std::future_status::timeout) {
-				std::cout << "wait time out\n";
-				return std::nullopt;
-			}
-		}
-		if (this->hasException()) {
-			return std::nullopt;
-		}
-		return this->future().get(); // 这会自动抛出存储的异常
-	}
+    // co_return, promise_type.return_value(total)
+    void return_value(T v)
+    {
+        std::cout << "return_value " << v << '\n';
+        this->set_value(std::move(v));
+    }
 };
 
 template <>
-class RyCoroTask<void> : public RyCoroTaskBase<void>
-{
-public:
-	using RyCoroTaskBase<void>::RyCoroTaskBase;
+struct RyCoroTaskPromise<void> : RyCoroTaskPromiseBase<void, true> {
+    RyCoroTask<void, true> get_return_object();
 
-	// 获取协程结果 - 会阻塞当前线程
-	void waitForFinished(std::chrono::milliseconds timeout) const
-	{
-		if (!this->m_coro.done()) {
-			auto status = this->m_coro.promise().get_future().wait_for(timeout);
-			if (status == std::future_status::timeout) {
-				std::cout << "wait time out\n";
-				return;
-			}
-		}
-	}
+    void return_void()
+    {
+        std::cout << "return_void\n";
+        this->set_value();
+    }
+};
+
+template <>
+struct RyCoroTaskPromise<void, false> : RyCoroTaskPromiseBase<void, false> {
+    RyCoroTask<void, false> get_return_object();
+
+    void return_void()
+    {
+        std::cout << "return_void\n";
+        this->set_value();
+    }
+};
+} // namespace RyCoro::detail
+template <typename T, bool InitialSuspend = true>
+class RyCoroTaskBase {
+public:
+    using promise_type = RyCoro::detail::RyCoroTaskPromise<T, InitialSuspend>;
+
+    RyCoroTaskBase(std::coroutine_handle<promise_type> h)
+        : m_detached(h.promise().detached)
+        , m_coro(h)
+    {
+    }
+    ~RyCoroTaskBase()
+    {
+        if (m_coro) {
+            if (m_coro.done()) {
+                std::cout << "destroy coro: " << m_coro.address() << '\n';
+                m_coro.destroy();
+            } else {
+                std::cout << "detach coro: " << m_coro.address() << '\n';
+                detach();
+            }
+        }
+    }
+    RyCoroTaskBase(const RyCoroTaskBase&) = delete;
+    RyCoroTaskBase& operator=(const RyCoroTaskBase&) = delete;
+
+    RyCoroTaskBase(RyCoroTaskBase&& other) noexcept
+        : m_detached(std::move(other.m_detached))
+        , m_coro(other.m_coro)
+    {
+        other.m_coro = nullptr;
+    }
+
+    RyCoroTaskBase& operator=(RyCoroTaskBase&& other) noexcept
+    {
+        if (this != &other) {
+            if (m_coro && !m_detached->load()) {
+                m_coro.destroy();
+            }
+            m_coro = other.m_coro;
+            m_detached = std::move(other.m_detached);
+            other.m_coro = nullptr;
+        }
+        return *this;
+    }
+
+    // 获取协程句柄
+    std::coroutine_handle<> handle() { return m_coro; }
+
+    // 获取 shared_future，可以用于异步获取结果
+    std::shared_future<T> future()
+    {
+        if (!m_future.valid()) {
+            m_future = m_coro.promise().get_future().share();
+        }
+        return m_future;
+    }
+
+    // 检查是否有异常
+    bool hasException() const { return m_coro.promise().exceptionPtr != nullptr; }
+
+    // 新增：获取异常（如果有）
+    std::exception_ptr exception() const { return m_coro.promise().exceptionPtr; }
+
+    //
+    void resume() const
+    {
+        if (this->m_coro && !this->m_coro.done() && !this->m_started) {
+            this->m_coro.resume();
+        }
+    }
+    //
+    void detach() const { m_detached->store(true); }
+
+    ///
+    bool await_ready() { return m_coro && m_coro.done(); }
+
+    T await_resume()
+    {
+        if (hasException()) {
+            return {};
+        }
+        return future().get();
+    }
+
+    // 挂起当前协程，并恢复子协程
+    std::coroutine_handle<> await_suspend(std::coroutine_handle<> waiter) noexcept
+    {
+        // 储存父协程的句柄
+        this->m_coro.promise().continuation = waiter;
+        if (!this->m_started) {
+            this->m_started = true;
+            return this->m_coro;
+        }
+        // 返回子协程的句柄，使其恢复执行
+        return std::noop_coroutine();
+    }
+
+protected:
+    std::shared_ptr<std::atomic<bool>> m_detached;
+    std::coroutine_handle<promise_type> m_coro;
+    std::shared_future<T> m_future; // 缓存 shared_future 对象
+    std::atomic<bool> m_started = !InitialSuspend; // 标记协程是否已经开始执行
+};
+
+template <typename T, bool InitialSuspend = true>
+class RyCoroTask : public RyCoroTaskBase<T, InitialSuspend> {
+public:
+    using RyCoroTaskBase<T, InitialSuspend>::RyCoroTaskBase;
+
+    // 获取协程结果 - 会阻塞当前线程
+    std::optional<T> waitForResult(std::chrono::milliseconds timeout = std::chrono::milliseconds(3000))
+    {
+        if (!this->m_coro) {
+            return std::nullopt;
+        }
+        if (!this->m_coro.done()) {
+            if (!this->m_started) {
+                this->resume();
+            }
+            auto status = this->future().wait_for(timeout);
+            if (status == std::future_status::timeout) {
+                std::cout << "wait time out\n";
+                return std::nullopt;
+            }
+        }
+        if (this->hasException()) {
+            return std::nullopt;
+        }
+        return this->future().get(); // 这会自动抛出存储的异常
+    }
+};
+
+template <bool InitialSuspend>
+class RyCoroTask<void, InitialSuspend> : public RyCoroTaskBase<void, InitialSuspend> {
+public:
+    using RyCoroTaskBase<void, InitialSuspend>::RyCoroTaskBase;
+
+    // 获取协程结果 - 会阻塞当前线程
+    void waitForFinished(std::chrono::milliseconds timeout) const
+    {
+        if (!this->m_coro.done()) {
+            auto status = this->m_coro.promise().get_future().wait_for(timeout);
+            if (status == std::future_status::timeout) {
+                std::cout << "wait time out\n";
+                return;
+            }
+        }
+    }
 };
 //
 
-namespace RyCoro::detail
+namespace RyCoro::detail {
+template <typename T, bool InitialSuspend>
+RyCoroTask<T, InitialSuspend> RyCoroTaskPromise<T, InitialSuspend>::get_return_object()
 {
-	template <typename T>
-	RyCoroTask<T> RyCoroTaskPromise<T>::get_return_object()
-	{
-		std::cout << "create coroutine task\n";
-		return RyCoroTask(std::coroutine_handle<RyCoroTaskPromise>::from_promise(*this));
-	}
+    std::cout << "create coroutine task\n";
+    return RyCoroTask(std::coroutine_handle<RyCoroTaskPromise>::from_promise(*this));
+}
 
-	inline RyCoroTask<void> RyCoroTaskPromise<void>::get_return_object()
-	{
-		std::cout << "coroutine task created" << '\n';
-		return RyCoroTask<void>(std::coroutine_handle<RyCoroTaskPromise>::from_promise(*this));
-	}
+inline RyCoroTask<void, true> RyCoroTaskPromise<void, true>::get_return_object()
+{
+
+    std::cout << "coroutine task created" << '\n';
+    return RyCoroTask<void, true>(std::coroutine_handle<RyCoroTaskPromise<void, true>>::from_promise(*this));
+}
+
+inline RyCoroTask<void, false> RyCoroTaskPromise<void, false>::get_return_object()
+{
+
+    std::cout << "coroutine task created" << '\n';
+    return RyCoroTask<void, false>(std::coroutine_handle<RyCoroTaskPromise<void, false>>::from_promise(*this));
+}
+//
+// template <bool InitialSuspend>
+// inline RyCoroTask<void> RyCoroTaskPromise<void, true>::get_return_object()
+//{
+//    std::cout << "coroutine task created" << '\n';
+//    return RyCoroTask<void>(std::coroutine_handle<RyCoroTaskPromise<void, true>>::from_promise(*this));
+//}
 } // namespace RyCoro::detail
 
 // 时间等待器
-class RyTimeAwaitable
-{
+class RyTimeAwaitable {
 public:
-	RyTimeAwaitable(std::chrono::microseconds duration)
-		: m_delay(duration)
-	{ }
+    RyTimeAwaitable(std::chrono::microseconds duration)
+        : m_delay(duration)
+    {
+    }
 
-	bool await_ready() const noexcept { return false; }
+    bool await_ready() const noexcept { return false; }
 
-	void await_suspend(std::coroutine_handle<> h)
-	{
-		std::cout << "pause, wait: " << m_delay.count() << "us" << '\n';
-		std::thread([this, h]() mutable {
-			std::this_thread::sleep_for(m_delay);
-			h.resume();
-			}).detach();
-	}
+    void await_suspend(std::coroutine_handle<> h)
+    {
+        std::cout << "pause, wait: " << m_delay.count() << "us" << '\n';
+        std::thread([this, h]() mutable {
+            std::this_thread::sleep_for(m_delay);
+            h.resume();
+        }).detach();
+    }
 
-	void await_resume() const noexcept { std::cout << "timeout, resume\n"; }
+    void await_resume() const noexcept { std::cout << "timeout, resume\n"; }
 
 private:
-	std::chrono::microseconds m_delay;
+    std::chrono::microseconds m_delay;
 };
 
-namespace RyCoro
+namespace RyCoro {
+inline RyTimeAwaitable delay(std::chrono::microseconds duration)
 {
-	inline RyTimeAwaitable delay(std::chrono::microseconds duration)
-	{
-		return RyTimeAwaitable(duration);
-	}
+    return RyTimeAwaitable(duration);
+}
 } // namespace RyCoro
 
 template <typename T>
-class FutureAwaiter
-{
+class FutureAwaiter {
 public:
-	FutureAwaiter(std::future<T>&& future)
-		: future_(std::move(future))
-	{ }
+    FutureAwaiter(std::future<T>&& future)
+        : future_(std::move(future))
+    {
+    }
 
-	bool await_ready() const noexcept { return future_.wait_for(std::chrono::seconds(0)) == std::future_status::ready; }
+    bool await_ready() const noexcept { return future_.wait_for(std::chrono::seconds(0)) == std::future_status::ready; }
 
-	void await_suspend(std::coroutine_handle<> h)
-	{
-		std::thread([this, h]() mutable {
-			future_.wait();
-			h.resume();
-			}).detach();
-	}
+    void await_suspend(std::coroutine_handle<> h)
+    {
+        std::thread([this, h]() mutable {
+            future_.wait();
+            h.resume();
+        }).detach();
+    }
 
-	T await_resume() { return future_.get(); }
+    T await_resume() { return future_.get(); }
 
 private:
-	std::future<T> future_;
+    std::future<T> future_;
 };
 
 template <typename T>
 FutureAwaiter<T> operator co_await(std::future<T>&& future)
 {
-	return FutureAwaiter<T>(std::move(future));
+    return FutureAwaiter<T>(std::move(future));
 }
--- main.cpp
+++ main.cpp
@@ -3,158 +3,166 @@
 //// 简单的随机数生成器
 class IntReader {
 public:
-	bool await_ready() {
-		return false;
-	}
+    bool await_ready()
+    {
+        return false;
+    }
 
-	void await_suspend(std::coroutine_handle<> handle) {
-		std::thread thread([this, handle]() {
-			std::cout << "await_suspend handle " << handle.address() << '\n';
-			std::srand(static_cast<unsigned int>(std::time(nullptr)));
-			std::this_thread::sleep_for(std::chrono::milliseconds{ 100 });
-			value_ = 1;
-			handle.resume();
-			});
-		thread.detach();
-	}
+    void await_suspend(std::coroutine_handle<> handle)
+    {
+        std::thread thread([this, handle]() {
+            std::cout << "await_suspend handle " << handle.address() << '\n';
+            std::srand(static_cast<unsigned int>(std::time(nullptr)));
+            std::this_thread::sleep_for(std::chrono::milliseconds { 100 });
+            value_ = 1;
+            handle.resume();
+        });
+        thread.detach();
+    }
 
-	int await_resume() {
-		return value_;
-	}
+    int await_resume()
+    {
+        return value_;
+    }
 
 private:
-	int value_{};
+    int value_ {};
 };
 
 // 协程函数，读取三个随机数并打印其总和
-RyCoroTask<int> PrintInt() {
-	IntReader reader1;
-	std::cout << "1\n";
-	int total = co_await reader1;
+RyCoroTask<int, false> PrintInt()
+{
+    IntReader reader1;
+    std::cout << "1\n";
+    int total = co_await reader1;
 
-	IntReader reader2;
-	std::cout << "2\n";
-	total += co_await reader2;
+    IntReader reader2;
+    std::cout << "2\n";
+    total += co_await reader2;
 
-	IntReader reader3;
-	std::cout << "3\n";
-	total += co_await reader3;
+    IntReader reader3;
+    std::cout << "3\n";
+    total += co_await reader3;
 
-	std::cout << "total: " << total << '\n';
-	co_return total;
-
+    std::cout << "total: " << total << '\n';
+    co_return total;
 }
 // 模拟可能抛出异常的协程
-RyCoroTask<int> mayThrowException(bool shouldThrow) {
-	co_await RyCoro::delay(std::chrono::milliseconds(100));
-	if (shouldThrow) {
-		throw std::runtime_error("Intentional exception");
-	}
-	co_return 42;
+RyCoroTask<int> mayThrowException(bool shouldThrow)
+{
+    co_await RyCoro::delay(std::chrono::milliseconds(100));
+    if (shouldThrow) {
+        throw std::runtime_error("Intentional exception");
+    }
+    co_return 42;
 }
 
 // void 类型的协程
-RyCoroTask<void> voidCoroutine() {
-	std::cout << "开始执行 void 协程\n";
-	co_await RyCoro::delay(std::chrono::milliseconds(100));
-	std::cout << "void 协程执行完毕\n";
+RyCoroTask<void> voidCoroutine()
+{
+    std::cout << "开始执行 void 协程\n";
+    co_await RyCoro::delay(std::chrono::milliseconds(100));
+    std::cout << "void 协程执行完毕\n";
 }
 
 // 长时间运行的协程
-RyCoroTask<int> longRunningTask() {
-	std::cout << "开始长时间运行的任务\n";
-	co_await RyCoro::delay(std::chrono::seconds(5));
-	std::cout << "长时间运行的任务完成\n";
-	co_return 100;
+RyCoroTask<int> longRunningTask()
+{
+    std::cout << "开始长时间运行的任务\n";
+    co_await RyCoro::delay(std::chrono::seconds(5));
+    std::cout << "长时间运行的任务完成\n";
+    co_return 100;
 }
 
-RyCoroTask<int> nestedCoroutine() {
-	auto innerResult = co_await PrintInt();
-	co_return innerResult * 2;
-}
+// RyCoroTask<int> nestedCoroutine()
+//{
+//     auto innerResult = co_await PrintInt();
+//     co_return innerResult * 2;
+// }
 
-int main() {
-	std::cout << "开始测试协程实现\n\n";
+int main()
+{
+    std::cout << "开始测试协程实现\n\n";
 
-	// 1. 测试基本的协程功能
-	std::cout << "1. 测试基本的协程功能：\n";
-	auto printIntTask = PrintInt();
-	auto result = printIntTask.waitForResult();
-	assert(result.has_value() && "PrintInt should return a value");
-	assert(*result == 3 && "PrintInt should return 3"); // 假设我们期望总和为3
-	std::cout << "PrintInt 测试通过\n\n";
+    // 1. 测试基本的协程功能
+    std::cout << "1. 测试基本的协程功能：\n";
+    auto printIntTask = PrintInt();
+    auto result = printIntTask.waitForResult();
+    assert(result.has_value() && "PrintInt should return a value");
+    assert(*result == 3 && "PrintInt should return 3"); // 假设我们期望总和为3
+    std::cout << "PrintInt 测试通过\n\n";
 
-	// 2. 测试异常处理
-	std::cout << "2. 测试异常处理：\n";
-	auto exceptionTask = mayThrowException(true);
-	exceptionTask.waitForResult();
-	assert(exceptionTask.hasException() && "Exception should have been thrown");
-	std::cout << "异常处理测试通过\n\n";
+    // 2. 测试异常处理
+    std::cout << "2. 测试异常处理：\n";
+    auto exceptionTask = mayThrowException(true);
+    exceptionTask.waitForResult();
+    assert(exceptionTask.hasException() && "Exception should have been thrown");
+    std::cout << "异常处理测试通过\n\n";
 
-	// 3. 测试超时机制
-	std::cout << "3. 测试超时机制：\n";
-	auto timeoutTask = longRunningTask();
-	auto timeoutResult = timeoutTask.waitForResult(std::chrono::seconds(1));
-	assert(!timeoutResult.has_value() && "Task should have timed out");
-	std::cout << "超时机制测试通过\n\n";
+    // 3. 测试超时机制
+    std::cout << "3. 测试超时机制：\n";
+    auto timeoutTask = longRunningTask();
+    auto timeoutResult = timeoutTask.waitForResult(std::chrono::seconds(1));
+    assert(!timeoutResult.has_value() && "Task should have timed out");
+    std::cout << "超时机制测试通过\n\n";
 
-	// 4. 测试void类型的协程
-	std::cout << "4. 测试void类型的协程：\n";
-	auto voidTask = voidCoroutine();
-	voidTask.waitForFinished(std::chrono::seconds(2));
-	// 对于void协程，我们只能测试它是否正常完成，无法断言具体的返回值
-	std::cout << "void协程测试完成\n\n";
+    // 4. 测试void类型的协程
+    std::cout << "4. 测试void类型的协程：\n";
+    auto voidTask = voidCoroutine();
+    voidTask.waitForFinished(std::chrono::seconds(2));
+    // 对于void协程，我们只能测试它是否正常完成，无法断言具体的返回值
+    std::cout << "void协程测试完成\n\n";
 
-	// 5. 测试协程的分离（detach）功能
-	std::cout << "5. 测试协程的分离（detach）功能：\n";
-	{
-		auto detachedTask = longRunningTask();
-		detachedTask.detach();
-		std::cout << "协程已分离，主函数继续执行\n";
-	}
-	std::this_thread::sleep_for(std::chrono::seconds(6));
-	std::cout << "分离的协程应该已经完成\n\n";
+    // 5. 测试协程的分离（detach）功能
+    std::cout << "5. 测试协程的分离（detach）功能：\n";
+    {
+        auto detachedTask = longRunningTask();
+        detachedTask.detach();
+        std::cout << "协程已分离，主函数继续执行\n";
+    }
+    std::this_thread::sleep_for(std::chrono::seconds(6));
+    std::cout << "分离的协程应该已经完成\n\n";
 
-	// 6. 测试协程的取消（通过超时）
-	std::cout << "6. 测试协程的取消（通过超时）：\n";
-	{
-		auto cancelTask = longRunningTask();
-		auto cancelResult = cancelTask.waitForResult(std::chrono::milliseconds(100));
-		assert(!cancelResult.has_value() && "Task should have been cancelled due to timeout");
-		std::cout << "协程取消测试通过\n\n";
-	}
+    // 6. 测试协程的取消（通过超时）
+    std::cout << "6. 测试协程的取消（通过超时）：\n";
+    {
+        auto cancelTask = longRunningTask();
+        auto cancelResult = cancelTask.waitForResult(std::chrono::milliseconds(100));
+        assert(!cancelResult.has_value() && "Task should have been cancelled due to timeout");
+        std::cout << "协程取消测试通过\n\n";
+    }
 
-	// 7. 测试正常完成的协程
-	std::cout << "7. 测试正常完成的协程：\n";
-	auto normalTask = mayThrowException(false);
-	auto normalResult = normalTask.waitForResult();
-	assert(normalResult.has_value() && "Task should complete normally");
-	assert(*normalResult == 42 && "Task should return 42");
-	std::cout << "正常完成的协程测试通过\n\n";
+    // 7. 测试正常完成的协程
+    std::cout << "7. 测试正常完成的协程：\n";
+    auto normalTask = mayThrowException(false);
+    auto normalResult = normalTask.waitForResult();
+    assert(normalResult.has_value() && "Task should complete normally");
+    assert(*normalResult == 42 && "Task should return 42");
+    std::cout << "正常完成的协程测试通过\n\n";
 
-	// 8. 测试多个协程并发执行
-	std::cout << "8. 测试多个协程并发执行：\n";
-	auto task1 = PrintInt();
-	auto task2 = PrintInt();
-	auto task3 = PrintInt();
+    // 8. 测试多个协程并发执行
+    std::cout << "8. 测试多个协程并发执行：\n";
+    auto task1 = PrintInt();
+    auto task2 = PrintInt();
+    auto task3 = PrintInt();
 
-	auto result1 = task1.waitForResult();
-	auto result2 = task2.waitForResult();
-	auto result3 = task3.waitForResult();
+    auto result1 = task1.waitForResult();
+    auto result2 = task2.waitForResult();
+    auto result3 = task3.waitForResult();
 
-	assert(result1.has_value() && result2.has_value() && result3.has_value() && "All tasks should complete");
-	assert(*result1 == 3 && *result2 == 3 && *result3 == 3 && "All tasks should return 3");
-	std::cout << "多个协程并发执行测试通过\n\n";
+    assert(result1.has_value() && result2.has_value() && result3.has_value() && "All tasks should complete");
+    assert(*result1 == 3 && *result2 == 3 && *result3 == 3 && "All tasks should return 3");
+    std::cout << "多个协程并发执行测试通过\n\n";
 
-	// 9. 测试协程的嵌套
-	std::cout << "9. 测试协程的嵌套：\n";
-	auto nestedTask = nestedCoroutine();
-	auto nestedResult = nestedTask.waitForResult();
-	assert(nestedResult.has_value() && "Nested coroutine should complete");
-	assert(*nestedResult == 6 && "Nested coroutine should return 6");
-	std::cout << "协程嵌套测试通过\n\n";
+    // 9. 测试协程的嵌套
+    // std::cout << "9. 测试协程的嵌套：\n";
+    // auto nestedTask = nestedCoroutine();
+    // auto nestedResult = nestedTask.waitForResult();
+    // assert(nestedResult.has_value() && "Nested coroutine should complete");
+    // assert(*nestedResult == 6 && "Nested coroutine should return 6");
+    // std::cout << "协程嵌套测试通过\n\n";
 
-	std::cout << "所有测试完成，测试通过！\n";
+    std::cout << "所有测试完成，测试通过！\n";
 
-	return 0;
+    return 0;
 }
